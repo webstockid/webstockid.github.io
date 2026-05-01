42d7c4b2acc385b8159f4634834adcbfcbd09f22
Stock's ID <hckfb22@gmail.com>
2026-05-01 23:57:04 +0700

    Add files via upload

--- stockid_script/ratting.js
+++ stockid_script/ratting.js
@@ -1,52 +1,52 @@
 const data = [
 {
 	nama: "Mas. Purna",
-	positif: "Watchlist swing banyak, bisa bagger jika sabar. <br>Aku sangat makasih sama Mas Har ajarin Aku dari 0 sampai bisa saat ini kenal saham, Teknikal Support & Entry Safe.",
-	negatif: "Menurut aku sendiri kebanyakan Watchlist, mungkin nextnya dikurangin biar member bisa pokus dengan 1 emiten, biar ga pusing.<br>Kadang suruh cicil atau UW atau apalah itu, bagi yang belum terlalu paham mungkin bingung, mungkin bisa di sederhanakan (Bandar/Smartmoney).",
+	positif: "Watchlist swing banyak, bisa bagger jika sabar. <br>Aku sangat makasih sama Mas Har ajarin Aku dari 0 sampai bisa saat ini kenal Saham, Teknikal Support & Entry Safe.",
+	negatif: "Menurut Aku sendiri kebanyakan Watchlist, mungkin nextnya dikurangin biar Member bisa pokus dengan 1 Emiten, biar ga pusing.<br>Kadang suruh cicil atau UW atau apalah itu, bagi yang belum terlalu paham mungkin bingung, mungkin bisa di sederhanakan (Bandar/Smartmoney).",
 },
 {
 	nama: "Mas. Jefri",
-	positif: "Watchlist nya lumayan lah udah ga ngawur, dermawan sama member apalagi kemarin pas crash.",
-	negatif: "Buat Mas Har sih kurang²in Tlp Grup sambil berak.<br>Kalo buat Mas Irfan harus lebih sering ngoceh ke member yg baru nyebur ke saham takutnya mentalnya down krn ikut Watchlist tp ga untung malah buntung.",
+	positif: "Watchlist nya lumayan lah udah ga ngawur, dermawan sama Member apalagi kemarin pas crash.",
+	negatif: "Buat Mas Har sih kurang²in Tlp Grup sambil berak.<br>Kalo buat Mas Irfan harus lebih sering ngoceh ke Member yg baru nyebur ke Saham tAkutnya mentalnya down krn ikut Watchlist tp ga untung malah buntung.",
 },
 {
 	nama: "Mas. Tama",
-	positif: "Dari kesan positif jujur waktu awal kali sampai terakhir kemarin itu sangat berkesan sih kebaikannya, terus juga secara waktu masih mau terlibat dalam kesusahan membernya, sama² merasakan Trading Halt. 🗿<br>Sehingga yaa oke aja, terus juga baiklah, dalam artian di tempat lain kalau ada kejadian Trading Halt kemarin itu, pasti ya ga nambah secara gratis kelasnya, tapi ttp harus bayar.",
-	negatif: "Kadang Telp Grup ga jelas ngawani boker. 🗿<br>Tapi aku lebih ke Bang Irfan sih yg jarang nimbrung kaya menurut aku jadi ada jarak antara member dan Mentornya, kalau di perusahaan itu kurang sehat biasanya kalau ada yg kaya gitu pimpinannya.",
+	positif: "Dari kesan positif jujur waktu awal kali sampai terakhir kemarin itu sangat berkesan sih kebaikannya, terus juga secara waktu masih mau terlibat dalam kesusahan Membernya, sama² merasakan Trading Halt. 🗿<br>Sehingga yaa oke aja, terus juga baiklah, dalam artian di tempat lain kalau ada kejadian Trading Halt kemarin itu, pasti ya ga nambah secara gratis kelasnya, tapi ttp harus bayar.",
+	negatif: "Kadang Telp Grup ga jelas ngawani boker. 🗿<br>Tapi Aku lebih ke Bang Irfan sih yg jarang nimbrung kaya menurut Aku jadi ada jarak antara Member dan Mentornya, kalau di perusahaan itu kurang sehat biasanya kalau ada yg kaya gitu pimpinannya.",
 },
 {
 	nama: "Mas. Pramudya",
-	positif: "Banyak materi yang diajarkan dan menggunakan kata² yang mudah dipahami.",
-	negatif: "Negatif nya jujur yaaaa kata² Bang Har kalo di chat beda banget sama di telp (kalo gw lebih suka di telp sih).<br>Kalo di telp tuh kaya langsung masuk ke otak, kalo di chat itu ½ masuk otak ½ keluar telinga kanan hehehehehe.<br>Karna gw main saham ini baru² aja, gw tuh kadang bingung mau belajar apa dan kalo denger istilah baru di saham mesti langsung tanya/telp Bang Har.",
+	positif: "Banyak Materi yang diajarkan dan menggunakan kata² yang mudah dipahami.",
+	negatif: "Negatif nya jujur yaaaa kata² Bang Har kalo di chat beda banget sama di telp (kalo gw lebih suka di telp sih).<br>Kalo di telp tuh kaya langsung masuk ke otak, kalo di chat itu ½ masuk otak ½ keluar telinga kanan hehehehehe.<br>Karna gw main Saham ini baru² aja, gw tuh kadang bingung mau belajar apa dan kalo denger istilah baru di Saham mesti langsung tanya/telp Bang Har.",
 	netral: "Mungkin saran gw pas ada waktu lumayan luang, lebih banyak sharing² di grup/di WL juga gapapaaaaa biar anggota baru juga bisa belajar dari awal."
 },
 {
 	nama: "Mba. Zulia",
-	positif: "Untuk pembelajannya sudah bagus, admin bener² maintenence member dengan memberi ilmu yang mudah dimengerti, WL yang diberi juga bagus.",
-	negatif: "Tidak ada modul (pembelajaran yang pasti) sehingga untuk member yang baru terjun ke dunia saham akan sangat kebingungan untuk bertanya karna tidak ada materi.",
-	netral: "Dibuatkan modul untuk member baru sehingga saat diskusi bisa tau arah pembicaraannya atau agar punya bahan untuk bertanya dan bisa ditambah link untuk materi."
+	positif: "Untuk pembelajannya sudah bagus, Admin bener² maintenence Member dengan Memberi ilmu yang mudah dimengerti, WL yang diberi juga bagus.",
+	negatif: "Tidak ada modul (pembelajaran yang pasti) sehingga untuk Member yang baru terjun ke dunia Saham akan sangat kebingungan untuk bertanya karna tidak ada Materi.",
+	netral: "Dibuatkan modul untuk Member baru sehingga saat diskusi bisa tau arah pembicaraannya atau agar punya bahan untuk bertanya dan bisa ditambah link untuk Materi."
 },
 {
 	nama: "Mba. Febrina",
-	positif: "1. Penjelasan to the point dan mudah di pahami.<br>2. Materi banyak, terkadang memang bingung apa yg mau ditanyain tapi Admin tetep kasih materi tanpa harus tanya dulu.<br>3. Bersedia ngasih kelas tambahan saat IHSG Crash (meski bkn tanggung jawab Admin, dan ini udah baik banget).<br>4. Admin sabar, ramah dan termasuk fast respon kalau kita lg butuh.",
-	negatif: "Blm kepikiran, saya selalu Khusnudzon. 🤣",
+	positif: "1. Penjelasan to the point dan mudah di pahami.<br>2. Materi banyak, terkadang memang bingung apa yg mau ditanyain tapi Admin tetep kasih Materi tanpa harus tanya dulu.<br>3. Bersedia ngasih kelas tambahan saat IHSG Crash (meski bkn tanggung jawab Admin, dan ini udah baik banget).<br>4. Admin sabar, ramah dan termasuk fast respon kalau kita lg butuh.",
+	negatif: "Blm kepikiran, Saya selalu Khusnudzon. 🤣",
 },
 {
 	nama: "Mas. Ilham",
-	netral: "Kalau untuk kesan positip negatip si gaada si, malah menurut aku grub enak pembelajaran tuh kek gini, kadang nyantai, kadang marah, kadang lucu, campur campur, jadi enak.<br>Kesannya di member itu nyaman gitu, buat tempat cerita juga, cuma pandangan aku pas awal masuk grub VIP, jujur pas awal awal join VIP aku bingung, mau mulai dari mana, soalnya kan aku basic investasi juga belum ada.<br>Dulu mikirnya ikut kelas gini tuh nggak belajar, kita tuh tinggal entry doang, udah beres.<br>Ehh ternyata nggaa, terus aku radak bingung lah, nyari² basic investasi lah, basic trading lah, semua ku liat di Youtube.<br>Nahhh, saranku aja bang, buatin vidio basic basic aja bang untuk anak VIP yang baru baru besok biar kalo ada yang nanya mulu tinggal kasih video nya langsung liat basicnya biar ga bingung. 😭😭"
+	netral: "Kalau untuk kesan positip negatip si gaada si, malah menurut Aku grub enak pembelajaran tuh kek gini, kadang nyantai, kadang marah, kadang lucu, campur campur, jadi enak.<br>Kesannya di Member itu nyaman gitu, buat tempat cerita juga, cuma pandangan Aku pas awal masuk grub VIP, jujur pas awal awal join VIP Aku bingung, mau mulai dari mana, soalnya kan Aku basic investasi juga belum ada.<br>Dulu mikirnya ikut kelas gini tuh nggak belajar, kita tuh tinggal entry doang, udah beres.<br>Ehh ternyata nggaa, terus Aku radak bingung lah, nyari² basic investasi lah, basic trading lah, semua ku liat di Youtube.<br>Nahhh, saranku aja bang, buatin vidio basic basic aja bang untuk anak VIP yang baru baru besok biar kalo ada yang nanya mulu tinggal kasih video nya langsung liat basicnya biar ga bingung. 😭😭"
 },
 {
 	nama: "Mas. Rafli",
-	positif: "Watchlist sama pembelajaran bener² bermanfaat, semenjak gabung disni portofolioku bertambah dan cara memandang suatu emiten jadi berubah.",
+	positif: "Watchlist sama pembelajaran bener² bermanfaat, semenjak gabung disni portofolioku bertambah dan cara memandang suatu Emiten jadi berubah.",
 },
 {
 	nama: "Mas. Ardika",
-	positif: "Saya bergabung di Stockbit masih terbilang baru, September tahun lalu, terus sebulan ga gabung kelas, aku belajar otodidak, agak pusing sih.<br>Terus aku kenal Stock ID VIP ini dari Bang Hardi lewat Tiktok, terus aku masuk group biasa aja ga berbayar, terus belajar banyak disana, dan aku ga lama habistu gabung lah di VIP nya, dan Alhamdulillah disana aku belajar banyak, mulai dari Bang Hardi yg banyak bantu aku, yg nyediakan waktu buat nelpon belajar saham, ngasih Watchlist yg bagus.<br>Kemudian untuk Bang Irfan, dia ini ngasih Watchlist juga, dan dia yg ngajarin saham terutama yg untuk Trading.",
+	positif: "Saya bergabung di Stockbit masih terbilang baru, September tahun lalu, terus sebulan ga gabung kelas, Aku belajar otodidak, agak pusing sih.<br>Terus Aku kenal Stock ID VIP ini dari Bang Hardi lewat Tiktok, terus Aku masuk group biasa aja ga berbayar, terus belajar banyak disana, dan Aku ga lama habistu gabung lah di VIP nya, dan Alhamdulillah disana Aku belajar banyak, mulai dari Bang Hardi yg banyak bantu Aku, yg nyediakan waktu buat nelpon belajar Saham, ngasih Watchlist yg bagus.<br>Kemudian untuk Bang Irfan, dia ini ngasih Watchlist juga, dan dia yg ngajarin Saham terutama yg untuk Trading.",
 	netral: "Untuk Bang Hardi mungkin agak lebih sabar lagi menghadapi ritel yg agak keras kepala ini, dan sering nyangkut ini. 🗿<br>Perbanyak sabar ye Bang. 🗿🤩<br>Untuk Bang Irfan kek nya ga ada, dia udah sabar wkwk."
 },
 {
 	nama: "Mas. Hendra Simanjuntak",
-	positif: "Memiliki dua mentor yg positif mendukung member adalah suatu hal yg baik untuk saya yg baru gabung pasar saham.<br>Dengan 4 bulan saya diajari diberi waktu diskusi, mungkin kalo waktu itu ga lihat Tiktok Bang Irfan gabakal ketemu Bang Har, berkomunikasi dua arah dengan mentor sangat memberikan kesan positif dimana masih bisa memberikan penjelasan, sehat-sehat mentor.",
+	positif: "Memiliki dua Mentor yg positif mendukung Member adalah suatu hal yg baik untuk Saya yg baru gabung pasar Saham.<br>Dengan 4 bulan Saya diajari diberi waktu diskusi, mungkin kalo waktu itu ga lihat Tiktok Bang Irfan gabakal ketemu Bang Har, berkomunikasi dua arah dengan Mentor sangat Memberikan kesan positif dimana masih bisa Memberikan penjelasan, sehat-sehat Mentor.",
 	negatif: "Membernya kek Dajjal dan suka di Prank Berak, itu aja bang. 🤟🏻",
 },
 {
@@ -57,18 +57,48 @@ const data = [
 },
 {
 	nama: "Mas. Zidan",
-	positif: "Memberikan Insight yang bagus dan bermanfaat, apabila terjadi Crash tidak meninggalkan malah memberikan kelas tambahan gratis.",
+	positif: "Memberikan Insight yang bagus dan bermanfaat, apabila terjadi Crash tidak meninggalkan malah Memberikan kelas tambahan gratis.",
 },
 {
 	nama: "Mas. Ulil",
 	positif: "Bisa diajak diskusi lewat Telepon, bisa minta dijelaskan secara langsung, Watchlist enak buat analisa ulang.",
-	negatif: "Gapernah mabar ML sama saya. 🗿",
+	negatif: "Gapernah mabar ML sama Saya. 🗿",
 },
 {
 	nama: "Mas. Nofal",
-	positif: "Penyampaian gampang dicerna dan kedua mentor sangat baik dan sopan santun, sama sangat softpoken.",
+	positif: "Penyampaian gampang dicerna dan kedua Mentor sangat baik dan sopan santun, sama sangat softpoken.",
 	negatif: "Digoblok goblokin. 🗿🙏🏻",
 },
+{
+	nama: "Mas. Agung",
+	positif: "Pokoknya pertama masuk Stock ID VIP belum tau apa apa, sekarang sering dikasih tips dan masukan sama Mas Irfan dan Ko Hardi sudah mulai berisi. 🙏🏻🔥",
+	netral: "Ga ada kritik Stock ID VIP soalnya Mentornya bisa diajak mabar. 🤣🔥",
+},
+{
+	nama: "Mba. Putri Sharatu",
+	positif: "Jam belajar Fleksibel, selagi Mentor free jam berapapun gas.<br>Menu gacor semua kalo Kak Hardi Swing/Long Term “sabar bisa bagger”.<br>Kalo Kak Irfan “sat set bisa cuan” Day Trade/Tek Tok.<br>Penjelasan dari Kak Hardi sangat detail dan sabar, buat pemula yang kosong emang rekomended banget Join Kelas disini.<br>Kak Irfan adalah Founder FCA yang keren.",
+	negatif: "Kalo orang baperan pasti sedih denger kritikannya.",
+},
+{
+	nama: "Mas. Fawas",
+	positif: "Penyampaian Materi sudah baik dan jelas, Mentor sudah mampu menjelaskan dan mengajarkan Saya tentang Saham dengan cara yang mudah dipahami, terutama bagi pemula untuk Saya.",
+	negatif: "Gak ada si untuk sekarang, ya masukan aja jangan bosan dan jangan jengkel ngelajarin murid kek Saya ini. 😁🙏🏻",
+},
+{
+	nama: "Mas. Muazin",
+	netral: "Saran nya WL dikasih keterangan buat Swing atau Scalping.<br>-Ingatkan jika WL sudah tidak bagus.<br>-Infokan jika waktunya TP biar saat Saham naik terus turun agar Member tidak panik.<br>-Kalau ada info Saham bagus dikasih keterangan nya kok bisa bagus karena apa, biar Member baru cepat pinter hehehe.",
+},
+{
+	nama: "Mba. Puspita",
+	negatif: "No Hards Feeling, tapi suka mental breakdance kalo dibilang Member goblok padahhal udh ikut lama hahahhaha.",
+	netral: "Please, infokan WL mu yg guacor itu jangan banyak-banyak agar Saya tidak split ke banyak Emiten.",
+},
+{
+	nama: "Mas. Roid",
+	positif: "Kesan Positif yang mungkin bisa jadi tak dimiliki oleh Admin diluar adalah terjalinnya komunikasi yang baik dari Admin dan Member sehingga Member yang tak memiliki pemahaman bisa diberi pemahaman dengan penjelasan yang mudah.",
+	negatif: "Gada, sejauh ini Adminnya baik semua.",
+},
+
 ];
 
 const container = document.getElementById("ratingContainer");
