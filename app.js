406920d246eaf18be352773e5184d51c8ce056df
Stock's ID <hckfb22@gmail.com>
2026-08-19 17:57:28 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -732,24 +732,24 @@ function renderAISignalUI(ticker, stockData, isCached) {
 
 		buktiEl.innerHTML = `
 			<li class="flex justify-between items-center bg-slate-900/60 p-2 rounded border border-slate-800/80">
-				<span>• Harga: <strong class="text-sky-300 font-mono font-bold">Rp${price.toLocaleString('id-ID')}</strong> (${stockData.changePct >= 0 ? '+' : ''}${stockData.changePct}%)</span>
+				<span>• Harga: <strong class="text-sky-500 font-mono font-bold">Rp${price.toLocaleString('id-ID')}</strong> (${stockData.changePct >= 0 ? '+' : ''}${stockData.changePct}%)</span>
 				<span class="text-[10px] lg:text-[11px] text-white">${isCached ? 'Cache Instant' : 'Live Data'}</span>
 			</li>
 			<li class="flex justify-between items-center bg-slate-900/60 p-2 rounded border border-slate-800/80">
 				<span>• Volume Transaksi:</span>
-				<span class="font-mono text-sky-300 font-bold">${(stockData.currentLot || 0).toLocaleString('id-ID')} Lot</span>
+				<span class="font-mono text-sky-500 font-bold">${(stockData.currentLot || 0).toLocaleString('id-ID')} Lot</span>
 			</li>
 			<li class="flex justify-between items-center bg-slate-900/60 p-2 rounded border border-slate-800/80">
 				<span>• Total Valuasi:</span>
-				<span class="font-mono text-sky-300 font-bold">Rp ${(stockData.currentValuation || 0).toLocaleString('id-ID')}</span>
+				<span class="font-mono text-sky-500 font-bold">Rp ${(stockData.currentValuation || 0).toLocaleString('id-ID')}</span>
 			</li>
 			<li class="flex justify-between items-center bg-slate-900/60 p-2 rounded border border-slate-800/80">
 				<span>• Terendah 20 Hari / Rentang Tertinggi:</span>
-				<span class="font-mono text-sky-300 font-bold">Rp ${stockData.low20.toLocaleString('id-ID')} / Rp ${stockData.high20.toLocaleString('id-ID')}</span>
+				<span class="font-mono text-sky-500 font-bold">Rp ${stockData.low20.toLocaleString('id-ID')} / Rp ${stockData.high20.toLocaleString('id-ID')}</span>
 			</li>
 			<li class="flex justify-between items-center bg-slate-900/60 p-2 rounded border border-slate-800/80">
 				<span>• Posisi Tren MA5 / MA10 / MA20:</span>
-				<span class="font-mono text-sky-300 font-bold">Rp ${stockData.ma5.toLocaleString('id-ID')} / ${stockData.ma10.toLocaleString('id-ID')} / ${stockData.ma20.toLocaleString('id-ID')}</span>
+				<span class="font-mono text-sky-500 font-bold">Rp ${stockData.ma5.toLocaleString('id-ID')} / ${stockData.ma10.toLocaleString('id-ID')} / ${stockData.ma20.toLocaleString('id-ID')}</span>
 			</li>
 			<li class="flex justify-between items-center bg-slate-900/60 p-2 rounded border border-slate-800/80">
 				<span>• Rasio Volume vs Rerata Harian:</span>
