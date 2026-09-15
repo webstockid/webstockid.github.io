524a88ab5c6d83cf81089d5b0595015331dff2f7
Stock's ID <hckfb22@gmail.com>
2026-09-15 08:16:27 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1017,31 +1017,31 @@ function renderAISignalUI(ticker, stockData, isCached) {
 		let bandarBarColor = "from-slate-600 via-slate-400 to-slate-300 shadow-[0_0_15px_rgba(148,163,184,0.4)]";
 		let bandarPct = 50;
 
-		if (stockData.changePct > 2 && stockData.price > stockData.ma10 && stockData.volRatio > 2) {
-			bandarStatus = "Masif Akumulasi 🐋";
+		if (stockData.changePct > 2 && stockData.price > stockData.ma5 && stockData.volRatio > 2) {
+			bandarStatus = "Masif Akumulasi 🐂";
 			bandarColor = "text-emerald-400";
 			bandarBarColor = "from-emerald-600 via-emerald-400 to-teal-400 shadow-[0_0_20px_rgba(52,211,153,0.5)]";
-			bandarPct = 90; 
-		} else if (stockData.changePct > 2 && stockData.price > stockData.ma5 && stockData.volRatio > 1) {
+			bandarPct = 95; 
+		} else if (stockData.changePct > 1 && stockData.price > stockData.ma5 && stockData.volRatio > 1) {
 			bandarStatus = "Akumulasi 🐳";
 			bandarColor = "text-emerald-400";
 			bandarBarColor = "from-emerald-600 via-emerald-400 to-teal-400 shadow-[0_0_20px_rgba(52,211,153,0.5)]";
-			bandarPct = 75; 
+			bandarPct = 80; 
 		} else if (stockData.changePct >= -5 && stockData.price > stockData.ma5 && stockData.price < stockData.ma10 && stockData.volRatio > 1) {
 			bandarStatus = "Netral ⚖️";
 			bandarColor = "text-amber-400";
 			bandarBarColor = "from-amber-600 via-amber-400 to-yellow-300 shadow-[0_0_15px_rgba(251,191,36,0.4)]";
 			bandarPct = 60; 
-		} else if (stockData.changePct >= -5 && stockData.price < stockData.ma5 && stockData.volRatio > 1) {
-			bandarStatus = "Mark Down (Uji Support) 📉";
+		} else if (stockData.changePct >= -5 && stockData.price < stockData.ma10 && stockData.volRatio > 1) {
+			bandarStatus = "Mark Down (Uji Support) 🛡️";
 			bandarColor = "text-cyan-400";
 			bandarBarColor = "from-cyan-600 via-cyan-400 to-blue-300 shadow-[0_0_15px_rgba(56,189,248,0.4)]";
-			bandarPct = 40; 
+			bandarPct = 35; 
 		} else if (stockData.changePct < -5 && stockData.price < stockData.ma10) {
 			bandarStatus = "Distribusi Kuat (Buangan) 🚨";
 			bandarColor = "text-rose-400";
 			bandarBarColor = "from-rose-600 via-rose-500 to-red-400 shadow-[0_0_20px_rgba(244,63,94,0.5)]";
-			bandarPct = 25; 
+			bandarPct = 20; 
 		} else {
 			// Fallback aman jika pergerakan biasa saja
 			bandarStatus = "Netral ⚖️";
