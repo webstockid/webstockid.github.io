48326ebf72abea7ec6067cd1bf531748a91e345d
Stock's ID <hckfb22@gmail.com>
2026-09-24 14:08:10 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1090,13 +1090,13 @@ function renderAISignalUI(ticker, stockData, isCached) {
 
 		let bandarStatus = "Konsolidasi / Normal ⚖️";
 		let bandarColor = "text-slate-400";
-		let bandarBarColor = "from-slate-600 via-slate-400 to-slate-300 shadow-[0_0_15px_rgba(148,163,184,0.4)]";
+		let bandarBarColor = "from-amber-600 via-amber-400 to-yellow-300 shadow-[0_0_15px_rgba(148,163,184,0.4)]";
 		let bandarPct = 50;
 
 		if (stockData.changePct > 2 && stockData.volRatio > 3) {
 			bandarStatus = "Masif Akumulasi 🐂";
 			bandarColor = "text-emerald-400";
-			bandarBarColor = "from-emerald-600 via-emerald-400 to-teal-400 shadow-[0_0_20px_rgba(52,211,153,0.5)]";
+			bandarBarColor = "from-green-600 via-green-400 to-emerald-400 shadow-[0_0_20px_rgba(52,211,153,0.5)]";
 			bandarPct = 95; 
 		} else if (stockData.changePct > 1 && stockData.volRatio > 2) {
 			bandarStatus = "Akumulasi 🐳";
